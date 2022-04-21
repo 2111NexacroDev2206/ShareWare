c431ba7d1d23a346ec92b70a6f881a879b8afdf5
KWONJYE <sheisdanmi@gmail.com>
2022-04-21 12:49:57 +0900

    주소록

--- src/main/java/org/kh/shareware/member/service/logic/memberServiceImpl.java
+++ src/main/java/org/kh/shareware/member/service/logic/memberServiceImpl.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.kh.shareware.member.domain.Member;
+import org.kh.shareware.member.domain.PageInfo;
 import org.kh.shareware.member.service.memberService;
 import org.kh.shareware.member.store.memberStore;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -36,6 +36,18 @@ public class memberServiceImpl implements memberService{
 	public List<Member> modalPrintAll() {
 		List<Member> mList = mStore.selectAllMember(sqlSession);
 		return mList;
+	}
+
+	@Override
+	public int getListCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Member> printAll(PageInfo pi) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
